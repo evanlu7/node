1c19906b3fa923f2dfe6ecf4986a4ea275cc1f49
Evan Lu <evanlu7@gmail.com>
2017-10-29 15:45:48 -0700

    heh

--- liri.js
+++ liri.js
@@ -1,8 +1,40 @@
+// Require twitter
+var Twitter = require ("twitter");
 var keys =require("./keys.js"); 
 var request = require ("request");
-var http = require ("http");
-var https =  require ("https");
-var twitter require ("twitter");
+var client = new Twitter ({
+  consumer_key: '<zoTn8Skv5oWCnADN6ZhAYLWvy>',
+  consumer_secret: '<6lNckhdsthjxSdZN29Oi7DXengl1ijBZhSvCn43JTHD19HbuQc>',
+  access_token_key: '<923074775676620800-dJkUm5vsL2qpJIxvokciALk36G6Rd3g>',
+  access_token_secret: '<	hPCUVtSA0qClKojgOZBB3Dnfis6ScAv13m1hC0EXuaWWI>',
+})
+
+
+// function UserSearch (username) {
+// 	this.username = username;
+
+var params = {screen_name: 'trey_bongz7'};
+
+this.getTweets = function () {
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+ 
+
+  if (!error) {
+    console.log(tweets);
+
+  }
+});
+}
+this.getTweets();
+
+
+// }
+
+
+
+// module.exports = UserSearch;
+// var yo = new UserSearch ("trey_bongz7");
+// yo.getTweets(); 
 
 
 
@@ -11,5 +43,6 @@ var twitter require ("twitter");
 
 
 
-var application = process.argv[2];
-var method = process.argv [3]
+
+// var application = process.argv[2];
+// var method = process.argv [3]
