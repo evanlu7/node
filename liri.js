b2330a59bdb8137b02a4980af89795045088cfb1
Evan Lu <evanlu7@gmail.com>
2017-10-29 15:09:35 -0700

    a

---
+++ liri.js
@@ -0,0 +1,15 @@
+var keys =require("./keys.js"); 
+var request = require ("request");
+var http = require ("http");
+var https =  require ("https");
+var twitter require ("twitter");
+
+
+
+
+
+
+
+
+var application = process.argv[2];
+var method = process.argv [3]
