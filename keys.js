9e8a426f338d86cadd02da36017224434f3800f1
Evan Lu <evanlu7@gmail.com>
2017-10-29 19:04:10 -0700

    added twitter functionality

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
-console.log('this is loaded');
-console.log (this.tweets);
-var Twitter = require ("twitter")
 
-var client = new Twitter ({
-  consumer_key: '<zoTn8Skv5oWCnADN6ZhAYLWvy>',
-  consumer_secret: '<6lNckhdsthjxSdZN29Oi7DXengl1ijBZhSvCn43JTHD19HbuQc>',
-  access_token_key: '<923074775676620800-dJkUm5vsL2qpJIxvokciALk36G6Rd3g>',
-  access_token_secret: '<	hPCUVtSA0qClKojgOZBB3Dnfis6ScAv13m1hC0EXuaWWI>',
-})
+console.log("this is loaded");
 
-// module.exports = twitterKeys;
+exports.twitterKeys = {
+  consumer_key: 'zoTn8Skv5oWCnADN6ZhAYLWvy',
+  consumer_secret: '6lNckhdsthjxSdZN29Oi7DXengl1ijBZhSvCn43JTHD19HbuQc',
+  access_token_key: '923074775676620800-dJkUm5vsL2qpJIxvokciALk36G6Rd3g',
+  access_token_secret: 'hPCUVtSA0qClKojgOZBB3Dnfis6ScAv13m1hC0EXuaWWI'
+};
+
+
+
 
--- liri.js
+++ liri.js
@@ -1,48 +1,25 @@
-// Require twitter
-var Twitter = require ("twitter");
-var keys =require("./keys.js"); 
+var fs = require ("fs");
 var request = require ("request");
-var client = new Twitter ({
-  consumer_key: '<zoTn8Skv5oWCnADN6ZhAYLWvy>',
-  consumer_secret: '<6lNckhdsthjxSdZN29Oi7DXengl1ijBZhSvCn43JTHD19HbuQc>',
-  access_token_key: '<923074775676620800-dJkUm5vsL2qpJIxvokciALk36G6Rd3g>',
-  access_token_secret: '<	hPCUVtSA0qClKojgOZBB3Dnfis6ScAv13m1hC0EXuaWWI>',
-})
+
+var Twitter = require('twitter');
+var twitterKey = require ("./keys.js");
+var client = new Twitter (twitterKey.twitterKeys);
+
+var Spotify = require ("spotify");
 
 
-// function UserSearch (username) {
-// 	this.username = username;
-
-var params = {screen_name: 'trey_bongz7'};
-
-this.getTweets = function () {
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
- 
-
+var params = {screen_name: 'trey_bongz7', count: 20};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
 
-  }
-});
+  	for (var i =0; i< tweets.length; i++) {
+    
+console.log("Most recent tweets:") 
+console.log(tweets[i].text);
+
 }
-this.getTweets();
-
-
-// }
-
-
-
-// module.exports = UserSearch;
-// var yo = new UserSearch ("trey_bongz7");
-// yo.getTweets(); 
+}
+});
 
 
 
-
-
-
-
-
-
-// var application = process.argv[2];
-// var method = process.argv [3]
