965b45e1014cfe423c83ab4bd9a87dd4599912b2
Evan Lu <evanlu7@gmail.com>
2017-10-29 22:56:27 -0700

    somewhat functional

--- liri.js
+++ liri.js
@@ -1,25 +1,22 @@
 var fs = require ("fs");
 var request = require ("request");
 
+
 var Twitter = require('twitter');
 var twitterKey = require ("./keys.js");
 var client = new Twitter (twitterKey.twitterKeys);
 
+var params = {screen_name: 'trey_bongz7', count: 20};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
 
-
-
-// var params = {screen_name: 'trey_bongz7', count: 20};
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (!error) {
-
-//   	for (var i =0; i< tweets.length; i++) {
+  	for (var i =0; i< tweets.length; i++) {
     
-// console.log("Most recent tweets:") 
-// console.log(tweets[i].text);
-
-// }
-// }
-// });
+console.log("Most recent tweets:") 
+console.log(tweets[i].text);
+}
+}
+});
 
 
 var Spotify = require ("node-spotify-api");
@@ -33,6 +30,9 @@ spotify.search({ type: 'track', query: params }, function(err, data) {
     return; 
   } else {
   	console.log("Artist: " + data.tracks.items[0].artists[0].name);
+  	console.log("Song: " + data.tracks.items[0].name);
+  	console.log("Album: " + data.tracks.items[0].album.name);
+  	console.log("Preview link: " + data.tracks.items[0].preview_url);
   }
 });
 
