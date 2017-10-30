2d1a341d18988c1ac4f6fbdf330ff6d166547aab
Evan Lu <evanlu7@gmail.com>
2017-10-29 23:43:59 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # node
-node
+
+## Synopsis
+
+This project was made with Node.js to switch between various modules.  A user can access tweets, search for a song on Spotify, and search the OMDB database for a movie.
+
+## Code Example
+
+<!---->functio call to switch between 3 modules
+function twitterFunction() {
+<!---->api call
+var Twitter = require('twitter');
+<!---->link to separate page with Twitter keys
+var twitterKey = require("./keys.js");
+<!---->storing in a client variable
+var client = new Twitter(twitterKey.twitterKeys);
+<!---->twitter npm package
+var params = { screen_name: 'trey_bongz7', count: 20 };
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+if (!error) {
+for (var i = 0; i < tweets.length; i++) {
+console.log("Most recent tweets:")
+console.log(tweets[i].text);
+}}
+});
+}
+
+## Motivation
+
+This was the week 11 hw assignment for the Coding Bootcamp at UCLA Extension.
+
+## Installation
+
+This project is publicly available on github.com/evanlu7
+
+## API Reference
+
+twitter: https://www.npmjs.com/package/twitter
+node-spotify-api: https://www.npmjs.com/package/node-spotify-api
+OMDB database
+
+## Tests
+
+I ain't learn that yet
+
+## Contributors
+
+Evan
+
+## License
+
--- liri.js
+++ liri.js
@@ -1,86 +1,85 @@
-var fs = require ("fs");
-var request = require ("request");
+var fs = require("fs");
+var request = require("request");
 var action = process.argv[2];
 
 
-function appSwitch (){
-	switch (action) {
+function appSwitch() {
+    switch (action) {
 
-		case "my-tweets":
-		twitterFunction();
-		break;
+        case "my-tweets":
+            twitterFunction();
+            break;
 
-		case "spotify-this-song":
-		spotifyFunction();
-		break;
+        case "spotify-this-song":
+            spotifyFunction();
+            break;
 
-		case "movie-this":
-		omdbFunction ();
-		break;
+        case "movie-this":
+            omdbFunction();
+            break;
 
-	}
+    }
 };
 
 
-function twitterFunction (){
+function twitterFunction() {
+    var Twitter = require('twitter');
+    var twitterKey = require("./keys.js");
+    var client = new Twitter(twitterKey.twitterKeys);
 
-var Twitter = require('twitter');
-var twitterKey = require ("./keys.js");
-var client = new Twitter (twitterKey.twitterKeys);
+    var params = { screen_name: 'trey_bongz7', count: 20 };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
 
-var params = {screen_name: 'trey_bongz7', count: 20};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
 
-  	for (var i =0; i< tweets.length; i++) {
-    
-console.log("Most recent tweets:") 
-console.log(tweets[i].text);
-}
-}
-});
-}
-
-function spotifyFunction (){
-var Spotify = require ("node-spotify-api");
-var spotifyKey = require ("./sKeys.js");
-var spotify = new Spotify (spotifyKey.spotifyKeys);
-var params = process.argv[3];
-var customError = "oh shit ya done goofed";
-
-spotify.search({ type: 'track', query: params }, function(err, data) {
-  if (err) {
-    console.log('Error occurred: ' + customError);
-    return; 
-  } else {
-  	console.log("Artist: " + data.tracks.items[0].artists[0].name);
-  	console.log("Song: " + data.tracks.items[0].name);
-  	console.log("Album: " + data.tracks.items[0].album.name);
-  	console.log("Preview link: " + data.tracks.items[0].preview_url);
-  }
-});
+                console.log("Most recent tweets:")
+                console.log(tweets[i].text);
+            }
+        }
+    });
 }
 
+function spotifyFunction() {
+    var Spotify = require("node-spotify-api");
+    var spotifyKey = require("./sKeys.js");
+    var spotify = new Spotify(spotifyKey.spotifyKeys);
+    var params = process.argv[3];
+    var customError = "oh shit ya done goofed";
 
-function omdbFunction () {
-	var request = require ("request");
-	var movieName = process.argv[3];
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-request(queryUrl, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    console.log(
-    	"Title:" + JSON.parse(body).Title,
-    	"\nRelease Year: " + JSON.parse(body).Year,
-    	"\nIMDB Rating:" + JSON.parse(body).imdbRating,
-    	"\nRotten Tomatoes Rating:" + JSON.parse(body).rottenTomatoes,
-    	"\nCountry:" + JSON.parse(body).Country,
-    	"\nLanguage:" + JSON.parse(body).Language,
-    	"\nPlot:" + JSON.parse(body).Plot,
-    	"\nActors:" + JSON.parse(body).Actors,
-    	);
-  }
-});
+    spotify.search({ type: 'track', query: params }, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + customError);
+            return;
+        } else {
+            console.log("Artist: " + data.tracks.items[0].artists[0].name);
+            console.log("Song: " + data.tracks.items[0].name);
+            console.log("Album: " + data.tracks.items[0].album.name);
+            console.log("Preview link: " + data.tracks.items[0].preview_url);
+        }
+    });
 }
 
 
-appSwitch ();
+function omdbFunction() {
+    var request = require("request");
+    var movieName = process.argv[3];
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+    request(queryUrl, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(
+                "Title:" + JSON.parse(body).Title,
+                "\nRelease Year: " + JSON.parse(body).Year,
+                "\nIMDB Rating:" + JSON.parse(body).imdbRating,
+                "\nRotten Tomatoes Rating:" + JSON.parse(body).rottenTomatoes,
+                "\nCountry:" + JSON.parse(body).Country,
+                "\nLanguage:" + JSON.parse(body).Language,
+                "\nPlot:" + JSON.parse(body).Plot,
+                "\nActors:" + JSON.parse(body).Actors,
+            );
+        }
+    });
+}
+
+
+appSwitch();
